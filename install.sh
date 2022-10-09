eb17722c9959527bac6d4d855bdda965bdadbe33
Sebastian Hjelm <melon.bag@hotmail.com>
2022-10-09 10:41:45 +0200

    Change to Swerty layout
    Also check if already installed before installing

--- install.sh
+++ install.sh
@@ -1,15 +1,26 @@
-#!/bin/zsh
+#!/bin/bash
+echo "Installing Swerty..."
+
 # Add custom key map to system file with swedish layouts.
-sudo zsh -c 'cat key_map.txt >> /usr/share/X11/xkb/symbols/se'
+if [ -z "$(grep "swerty" /usr/share/X11/xkb/symbols/se)" ]; then
+    sudo bash -c 'cat key_map.txt >> /usr/share/X11/xkb/symbols/se'
+fi
 
 # Add variant infomation to evdev.xml
-sudo sed -i '/>swe</{
-:a                          
-N                                                  
-/<variantList>/!ba                                                                  
-revdev_variant.lst
-}
-' /usr/share/X11/xkb/rules/evdev.xml
+if [ -z "$(grep "swerty" /usr/share/X11/xkb/rules/evdev.xml)" ]; then
+    sudo sed -i '/>swe</{
+    :a                          
+    N                                                  
+    /<variantList>/!ba                                                                  
+    revdev_variant.lst
+    }
+    ' /usr/share/X11/xkb/rules/evdev.xml
+fi
 
 # Add variant infomation to evdev.lst
-sudo sed -i '/\! variant/a US_swe		se: US (Swedish) ' /usr/share/X11/xkb/rules/evdev.lst
+if [ -z "$(grep "swerty" /usr/share/X11/xkb/rules/evdev.lst)" ]; then
+    sudo sed -i '/\! variant/a swerty		se: Swerty (Swedish) ' /usr/share/X11/xkb/rules/evdev.lst
+fi
+
+echo ""
+echo "Installation done!"
